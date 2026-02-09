4062e2078d7dc07eb89cca86fa1635372985fc51
Anton Cristian <anton.cristian2123@gmail.com>
2026-02-09 23:01:11 +0200

    removed translation

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,7 @@ import { Navbar } from 'components/layout/navbar';
 import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/shopify';
 import { baseUrl } from 'lib/utils';
-<<<<<<< HEAD
 import { ReactNode, Suspense } from 'react';
-=======
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-import { ReactNode, Suspense } from 'react';
-import { locales } from '../i18n';
->>>>>>> 7d0c5fc38a37eb5184071f4d615eb506bcff6830
 import './globals.css';
 const { SITE_NAME } = process.env;
 
@@ -27,38 +20,24 @@ export const metadata = {
   }
 };
 
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
+
 
 export default async function RootLayout({
   children,
-    params: { locale }
 }: {
   children: ReactNode;
-    params: { locale: string };
 }) {
   const cart = getCart();
 
-  const messages = await getMessages();
   return (
-<<<<<<< HEAD
     <html lang="en" className={`${GeistSans.variable} light`}>
-=======
-    <html lang={locale} className={`${GeistSans.variable} light`}>
->>>>>>> 7d0c5fc38a37eb5184071f4d615eb506bcff6830
       <body className="bg-[#fafafa] text-black selection:bg-teal-300 min-h-screen overflow-x-hidden">
         <CartProvider cartPromise={cart}>
           <Navbar />
           <main className='mt-32'>
             <Suspense>
-<<<<<<< HEAD
              {children}
-=======
-              <NextIntlClientProvider messages={messages}>
                {children}
-              </NextIntlClientProvider>
->>>>>>> 7d0c5fc38a37eb5184071f4d615eb506bcff6830
             </Suspense>
           </main>
             <Footer />
